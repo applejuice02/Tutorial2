9b6d78f192c4d4d24be0458d96435ac7a64f7d8b
applejuice02 <zandercorliss@yahoo.com>
2022-10-25 21:03:53 -0400

    Challenge commit

---
+++ Assets/Scripts/Move.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Move : MonoBehaviour
+{
+    private Vector3 startPosition;
+    [SerializeField]
+    private float frequency = 1f;
+    [SerializeField]
+    private float magnitude = 1f;
+    [SerializeField]
+    private float offset = 0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+      startPosition = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       transform.position = startPosition + transform.up * Mathf.Sin(Time.time * frequency + offset) * magnitude;
+    }
+}
--- Assets/Scripts/PlayerScript.cs
+++ Assets/Scripts/PlayerScript.cs
@@ -9,17 +9,29 @@ public class PlayerScript : MonoBehaviour
      private Rigidbody2D rd2d;
      public float speed;
     public Text score;
+    public Text WinText;
     private int scoreValue = 0;
+    private int lives = 3;
+    public Text LivesText;
+    public AudioClip musicClipTwo; 
+    public AudioSource musicSource;
+   
 
     // Start is called before the first frame update
     void Start()
     {
+       {
         rd2d = GetComponent<Rigidbody2D>();
         score.text = scoreValue.ToString();
+        WinText.text = " ";
+       LivesText.text = lives.ToString();      
+       lives = 3;
+       SetLivesText();
+       }
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
         float hozMovement = Input.GetAxis("Horizontal");
         float vertMovement = Input.GetAxis("Vertical");
@@ -32,10 +44,23 @@ public class PlayerScript : MonoBehaviour
         if (collision.collider.tag == "Coin")
         {
             scoreValue += 1;
-           score.text = scoreValue.ToString(); 
+           score.text = scoreValue.ToString();
             Destroy(collision.collider.gameObject);
+            if (scoreValue == 4)
+            {
+                WinText.text = "You Win! Created by Alexander Corliss";
+                
+            }
         }
+         else if (collision.collider.tag == ("Enemy"))
+        {
+            Destroy(collision.collider.gameObject);
+            lives = lives - 1;  
+            SetLivesText();
+        }  
+    
     }   
+
 
     private void OnCollisionStay2D(Collision2D collision)
     {
@@ -45,6 +70,31 @@ public class PlayerScript : MonoBehaviour
             {
                 rd2d.AddForce(new Vector2(0, 3),ForceMode2D.Impulse);
             }
+        }
+    }
+    
+void SetLivesText ()
+    {
+        LivesText.text = "Lives: " + lives.ToString ();
+        if (lives == 0)
+        {
+            Destroy(this.gameObject);
+            WinText.text = "You Lose...";
+
+            if (Input.GetKey("escape"))
+            {
+            Application.Quit();
+            }
+        }
+    }
+
+    void update()
+    {
+        if (scoreValue == 4)
+        {
+             musicSource.clip = musicClipTwo;
+
+          musicSource.Play();
         }
     }
 }
